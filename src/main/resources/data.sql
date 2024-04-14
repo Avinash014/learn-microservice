039b49a1e05ad7c71e9d2fc758673131516182ff
Avinash Kumar <avinash.kumar@cognizer.ai>
2024-04-14 22:03:10 +0530

    initializing h2 with some data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details (id, name, age) values(4,'Avinash',26)
+insert into user_details (id, name, age) values(5,'Gaurav',27)
+insert into user_details (id, name, age) values(6,'Deepak',28)
